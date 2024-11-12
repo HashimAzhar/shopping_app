5766b7487dc071f2bd69b699b4cbc5853f42ad4b
Hashim Azhar <146028070+HashimAzhar@users.noreply.github.com>
2024-11-13 00:02:23 +0500

    first commit

---
+++ lib/pages/onBoarding.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class onBoarding extends StatefulWidget {
+  const onBoarding({super.key});
+
+  @override
+  State<onBoarding> createState() => _onBoardingState();
+}
+
+class _onBoardingState extends State<onBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 234, 235, 231),
+      body: Container(
+        margin: EdgeInsets.only(top: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset("images/headphone.PNG"),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(
+                'Expore\nThe Best\nProducts',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(right: 20),
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                      color: Colors.black, shape: BoxShape.circle),
+                  child: Text(
+                    'Next',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
